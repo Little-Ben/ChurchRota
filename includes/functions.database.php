e66836a434b6a057a1bc4380e11707f90d218f24
little_ben <Little-Ben@users.noreply.github.com>
2014-09-11 14:23:40 +0000

    V2.5.2 (SVN only): provide username after first install, hint on login page
    V2.5.2 - SVN - Revision 44 - Benjamin Schmitt - 2014-09-11
    - provide username after first install, hint on login page (ticket #7)

--- includes/functions.database.php
+++ includes/functions.database.php
@@ -281,6 +281,10 @@ function updateDatabase() {
 			executeDbSql("update cr_settings set version = '2.5.1'");
 			notifyInfo(__FILE__,"db-update=" . $version . "->2.5.1",$_SESSION['userid']);	
 			insertStatistics("system",__FILE__,"db-update","2.5.1",$version);				
+		case "2.5.1":
+			executeDbSql("update cr_settings set version = '2.5.2'");
+			notifyInfo(__FILE__,"db-update=" . $version . "->2.5.2",$_SESSION['userid']);	
+			insertStatistics("system",__FILE__,"db-update","2.5.2",$version);				
 			
 			//todo in a later version:
 			//executeDbSql("alter table cr_settings CHANGE debug_mode verbose_statistics int(1) DEFAULT '0' "); 
